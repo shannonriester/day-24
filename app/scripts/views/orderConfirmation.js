9635869bc9c89e6c427e06247287331073f93469
Shannon Riester <shannonriester@gmail.com>
2016-07-17 09:29:04 -0500

    added styles to form page

--- app/scripts/views/orderConfirmation.js
+++ app/scripts/views/orderConfirmation.js
@@ -11,13 +11,20 @@ function renderConfirmation(){
         <input class="order-form-inputs" type="text" name="first name" placeholder="first name" />
         <input class="order-form-inputs" type="text" name="last name" placeholder="last name" />
         <input class="order-form-inputs" type="text" name="address" placeholder="address" />
-        <textarea name="special instructions" rows="8" cols="40"></textarea>
+        <textarea name="special instructions" placeholder="additional comments..."rows="8" cols="50"></textarea>
+        <div class="orderform-btns">
+          <button id="clearBtn">clear</button>
+          <button id="submit">submit</button>
+        </div>
       </form>
     `);
 
 
     $orderForm.find('#orderBtn').empty().on('click', function(){
       orderSession.save();
+      });
+    $orderForm.find('#clearBtn').on('click', function(){
+      $('order-form-input').val('');
       });
 
   return $orderForm;
